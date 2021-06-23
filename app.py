367fb1ed85d783997fd4f67747adf1ee64788996
Steve Griesmer <sgriesmer@comcast.net>
2021-06-23 20:51:29 +0000

    cosmetic change to app.py to test build

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home Testing TEsting</h3>"
+    html = "<h3>Sklearn Prediction Home Testing 1,2,3 </h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
